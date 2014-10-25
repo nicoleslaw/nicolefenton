896378e6375e8041c846d54cb18331c706e37a49
Max Fenton <max@maxfenton.com>
2014-10-25 19:20:02 -0400

    Fix for the newsletter signup.

--- site/snippets/newslettersignup.php
+++ site/snippets/newslettersignup.php
@@ -4,7 +4,7 @@
               <form action="https://tinyletter.com/nicoleslaw" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/nicoleslaw', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
                 <fieldset>
                   <label for="tlemail">Your email address</label>
-                  <input type="text" name="required email" id="tlemail" placeholder="Email address" class="email" type="text" value="" autocomplete="" />
+                  <input type="text" name="email" id="tlemail" placeholder="Email address" class="required email" type="text" value="" autocomplete="" />
                   <input type="hidden" value="1" name="embed"/>
                   <button type="submit" value="Subscribe">
                     <svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 20 20" enable-background="new 0 0 20 20" xml:space="preserve">
