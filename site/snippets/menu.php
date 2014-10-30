750cab81a0e9293bd1a583f097bd9ab0c0d278eb
Max Fenton <max@maxfenton.com>
2014-10-29 22:28:43 -0400

    Enforce trailing slashes.

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -3,7 +3,7 @@
 
         <ul class="menu cf">
         <?php foreach($pages->visible() as $p): ?>
-  <li><a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
+  <li><a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>/"><?php echo html($p->title()) ?></a></li>
         <?php endforeach ?>
 </ul>
 
